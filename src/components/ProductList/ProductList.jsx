fe46864fdec7b270734608cb41c6b6c3fea1c940
vargd <vargdarrell@gmai.com>
2023-04-28 13:42:23 +0200

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('https://inevergamebot.site:8000/web-data', {
+        fetch('http://188.225.60.157:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
